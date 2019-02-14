361aafc6021305598d51c19ddcf9d7382531589a
unknown <link8801@hotmail.com>
2019-02-14 00:42:11 -0500

    Added content to home page, headshot to images tried to add ref in route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Joel F Murphy' });
+  res.render('index', {
+    title: 'Joel F Murphy',
+    headshot: './images/headshot.png'
+  });
 });
 
 module.exports = router;
