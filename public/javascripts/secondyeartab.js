b59f0f7e47734d58f07bc20457a8155f5f34c833
Joel Murphy <200203095@student.georgianc.on.ca>
2019-10-31 23:42:15 -0400

    Will the alert show

--- public/javascripts/secondyeartab.js
+++ public/javascripts/secondyeartab.js
@@ -2,6 +2,7 @@ const secondYearTab = document.querySelector('#secondYearTab');
 
 (function() {
 	if (window.location.hash.indexOf("secondYearTab") > -1) {
+        alert('Look Out!');
         secondYearTab.click();
     }
 })();
