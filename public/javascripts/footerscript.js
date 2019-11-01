a578f6485980c95f841183994235998d1a36d6f9
Joel Murphy <200203095@student.georgianc.on.ca>
2019-10-31 23:13:27 -0400

    Updated Some text, added js to keep footer on bot

---
+++ public/javascripts/footerscript.js
@@ -0,0 +1,26 @@
+let screenH;
+let prevH;
+let mainMarginH;
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const mainTag = document.querySelector('main');
+const bodyInitH = document.querySelector('body').offsetHeight;
+const mainStyle = getComputedStyle(mainTag);
+function getAllHeights(){
+	screenH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	if (screenH != prevH) {
+		mainMarginH = parseInt(mainStyle.marginTop) + parseInt(mainStyle.marginBottom);
+		prevH = screenH;
+		if (screenH < bodyInitH) {
+			mainTag.style.height = "auto";
+		} else {
+			mainTag.style.height = (screenH - footer.offsetHeight - header.offsetHeight - mainMarginH) + "px";
+		}
+	}
+}
+
+(function() {
+	prevH = -1;
+	getAllHeights();
+	window.onresize = getAllHeights;
+})();
